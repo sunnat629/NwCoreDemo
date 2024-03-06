82544fbf4dfdd86fc84aa69393540b873897745f
Mohi <suncha629@gmail.com>
2024-03-06 13:12:57 +0100

    JS is working

--- shared/src/jsMain/kotlin/dev/sunnat629/kmm2/Platform.js.kt
+++ shared/src/jsMain/kotlin/dev/sunnat629/kmm2/Platform.js.kt
@@ -4,21 +4,34 @@ import Utils.timestampToHumanReadable
 import kotlinx.browser.document
 
 fun main() {
-    TimelineFetcher.startFetchingTimeline { nwTimeline ->
-        val timeline = nwTimeline?.time?.let { timestampToHumanReadable(it) }?: throw Exception("NO RESULT")
+        document.getElementById("button1")?.addEventListener("click", {
+            // Example of asynchronous operation, you might want to fetch data from an API
+            TimelineFetcher.startFetchingTimeline { nwTimeline ->
+                val timeline = nwTimeline?.time?.let { timestampToHumanReadable(it) } ?: "NO RESULT"
+                updateWebPageWithNewContent("timeDisplay", "Nw Timeline: $timeline")
+            }
+        })
 
-        updateWebPageWithNewContent("$timeline -- $nwTimeline")
-    }
+    document.getElementById("button2")?.addEventListener("click", {
+        // Simulate another fetching operation, you can adjust based on your logic
+        TimelineFetcher.stopFetchingTimeline()
+        updateWebPageWithNewContent("timeDisplay", "Stopped Fetching")
+    })
 
-
+    document.getElementById("button3")?.addEventListener("click", {
+        // Simulate another fetching operation, you can adjust based on your logic
+        ExpManifestFetcher.fetchedManifest("p78x44cv1rrm23lx") {
+            updateWebPageWithNewContent("manifestDisplay","EXP Name: ${it?.name}")
+        }
+    })
 }
 
-fun updateWebPageWithNewContent(content: String) {
+fun updateWebPageWithNewContent(elementId: String, content: String) {
     // Implement the logic to update your web page with the new content.
     // This could involve manipulating the DOM to display the new timeline content.
     println("New timeline content: $content")
     // For example, if you have a <div id="timeline"> in your HTML:
-    val timelineElement = document.getElementById("timeline")
+    val timelineElement = document.getElementById(elementId)
     timelineElement?.textContent = content
 }
 
